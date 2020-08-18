6011115abdf7301ed49bff45d387a347455bdd45
foxclore <foxclore@zoho.com>
2020-08-18 13:03:26 +0300

    Changed server logics

--- server/server.go
+++ server/server.go
@@ -1,45 +1,13 @@
 package server
 
 import (
-	"externos.io/console/auth"
-	"externos.io/console/publisher"
-	"externos.io/console/web"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"log"
-	"net/http"
 )
 
+var websiteURL string
+
 func Init(r *gin.Engine, c Config) {
-	SetRoot(r)
-	SetApi(r)
-	SetApp(r)
-	SetAuth(r, c)
-	SetCompany(r)
-}
-
-func SetRoot(r *gin.Engine) {
-	r.GET("/alive", func(c *gin.Context) {
-		c.Status(http.StatusOK)
-	})
-
-	r.GET("/", func(c *gin.Context) {
-		if tid, err := c.Cookie("devid"); err == nil {
-			log.Println(tid)
-			if t, uid := auth.AuthenticateCookie(tid); t {
-				if x, _ := publisher.GetPublisherByUID(uid); x {
-					c.HTML(http.StatusOK, "index.html", web.RenderIndex(uid))
-					return
-				} else {
-					c.Redirect(http.StatusTemporaryRedirect, "/create")
-					c.Abort()
-					return
-				}
-			}
-		}
-		fmt.Println("REDIRECTED")
-		c.Redirect(http.StatusTemporaryRedirect, "/login")
-		c.Abort()
-		return
-	})
+	websiteURL = c.WebsiteURL
+	setApi(r)
+	setPages(r)
 }
--- server/server_api.go
+++ server/server_api.go
@@ -1,22 +1,12 @@
 package server
 
 import (
-	"externos.io/console/auth"
-	"externos.io/console/web"
 	"github.com/gin-gonic/gin"
-	"log"
 	"net/http"
 )
 
-func SetApi(r *gin.Engine) {
-	r.GET("/apps", func(c *gin.Context) {
-		if cid, err := c.Cookie("devid"); err == nil {
-			if t, uid := auth.AuthenticateCookie(cid); t {
-				c.JSON(http.StatusOK, web.RenderApplicationTables(uid))
-				return
-			}
-		}
-		log.Println("Error: failed to get cookie")
-		c.JSON(http.StatusOK, gin.H{})
-	})
+func setApi(r *gin.Engine) {
+	r.GET("/alive", func(c *gin.Context) { c.Status(http.StatusOK) })
+
+	r.GET("/apps", handleApiApps)
 }
